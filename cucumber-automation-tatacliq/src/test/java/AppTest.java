5a86930c1f3c7faa8a89069709f0e6266dee152a
ramkumar120889 <ramkumar1208@gmail.com>
2025-07-28 12:02:53 +0000

    trial for edit

--- cucumber-automation-tatacliq/src/test/java/AppTest.java
+++ cucumber-automation-tatacliq/src/test/java/AppTest.java
@@ -1,5 +1,9 @@
 import org.junit.jupiter.api.Test;
+
+import com.example.App;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class AppTest {
 
@@ -9,5 +13,13 @@ public class AppTest {
         String expected = "Hello, World!";
         String actual = App.getGreeting(); // Replace with actual method to test
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMainMethod2() {
+        // Assuming App has a method that returns a string
+        String expected = "Hello, World1!";
+        String actual = App.getGreeting(); // Replace with actual method to test
+        assertNotEquals(expected, actual);
     }
 }
