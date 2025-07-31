df6d8422560f4969b24d8e112d1c78c948466692
kumaraspireddeveloper <kumarthesmart@gmail.com>
2025-07-31 13:29:20 +0000

    cucumber test file added and testrunner

---
+++ cucumber-automation-tatacliq/features/tatacliq_test.feature
@@ -0,0 +1,6 @@
+Feature: TataCliq Homepage
+
+    Scenario: User visits TataCliq homepage
+        Given the user opens the TataCliq homepage
+        When the homepage loads successfully
+        Then the user should see the TataCliq logo
---
+++ cucumber-automation-tatacliq/src/test/java/com/test/tatacliq/steps/TatacliqSteps.java
@@ -0,0 +1,27 @@
+package com.test.tatacliq.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TatacliqSteps {
+
+
+    @Given("the user opens the TataCliq homepage")
+    public void the_user_opens_the_TataCliq_homepage() {
+        // Code to navigate to the TataCliq homepage
+        System.out.println("User opened the TataCliq homepage.");
+    }
+
+    @When("the homepage loads successfully")
+    public void the_homepage_loads_successfully() {
+        // Code to perform a search for the specified product
+        System.out.println("Navigating to the TataCliq homepage...");
+    }
+
+    @Then("the user should see the TataCliq logo")
+    public void the_user_should_see_the_TataCliq_logo() {
+        // Code to verify that search results contain the specified product
+        System.out.println("Homepage loaded successfully.");
+    }
+}
